1415254fd4a8d10b5a509a926de9fe0808b5346b
ZeruxMiatoll <zeruxmiatoll@gmail.com>
2021-09-03 18:02:48 +0200

    added Lrng driver

---
+++ drivers/char/lrng/lrng_chacha20.h
@@ -0,0 +1,30 @@
+
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause */
+/*
+ * LRNG ChaCha20 definitions
+ *
+ * Copyright (C) 2016 - 2021, Stephan Mueller <smueller@chronox.de>
+ */
+
+#include <crypto/chacha.h>
+
+/* State according to RFC 7539 section 2.3 */
+struct chacha20_block {
+	u32 constants[4];
+	union {
+#define CHACHA_KEY_SIZE_WORDS (CHACHA_KEY_SIZE / sizeof(u32))
+		u32 u[CHACHA_KEY_SIZE_WORDS];
+		u8  b[CHACHA_KEY_SIZE];
+	} key;
+	u32 counter;
+	u32 nonce[3];
+};
+
+static inline void lrng_cc20_init_rfc7539(struct chacha20_block *chacha20v)
+{
+	/* String "expand 32-byte k" */
+	chacha20v->constants[0] = 0x61707865;
+	chacha20v->constants[1] = 0x3320646e;
+	chacha20v->constants[2] = 0x79622d32;
+	chacha20v->constants[3] = 0x6b206574;
+}
---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* |MoonBase|™ SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 |MoonBase|™ SMP PREEMPT Fri Sep 3 17:38:20 CEST 2021"
+#define LINUX_COMPILE_BY "jsx"
+#define LINUX_COMPILE_HOST "igloo"
+#define LINUX_COMPILER "Azure clang version 14.0.0 (https://github.com/llvm/llvm-project 1a18bb9270ce5f6af6633b717058b6b299436203), LLD 14.0.0"
